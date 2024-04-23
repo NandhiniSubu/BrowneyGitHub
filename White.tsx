672a66f104188727363f45444cb31867dc396b85
subramani <subramani6202@gmail.com>
2024-04-23 18:03:02 +0530

    --Changes

---
+++ White.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const White = () => {
+  return (
+    <View>
+      <Text>White</Text>
+    </View>
+  );
+};
+
+export default White;
+
+const styles = StyleSheet.create({});
